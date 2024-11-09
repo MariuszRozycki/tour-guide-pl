702575a10735d2089c224f7334063842426e93f5
MariuszRozycki <mariansc83@icloud.com>
2024-11-09 12:44:28 +0100

    Add styling changes in HomeSlider.tsx, HomeSlider.css, SingleOfferPage.css

--- src/components/Home/HomeSlider.tsx
+++ src/components/Home/HomeSlider.tsx
@@ -13,7 +13,7 @@ const HomeSlider = () => {
   const settings = {
     dots: false,
     infinite: true,
-    speed: 500,
+    speed: 1000,
     slidesToShow: 3,
     slidesToScroll: 1,
     swipe: true,
